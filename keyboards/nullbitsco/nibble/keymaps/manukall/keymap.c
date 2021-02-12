d9b33d9abb2e984a537aa47b2fc6e21335386997
Manuel Kallenbach <mk@manukall.de>
2021-02-12 16:17:41 +0100

    nibble again

--- keyboards/nullbitsco/nibble/keymaps/manukall/keymap.c
+++ keyboards/nullbitsco/nibble/keymaps/manukall/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_MA] = LAYOUT_ansi(
             KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_ESC,
     KC_MUTE, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
-    _______, MO(_FN), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
+    KC_LGUI, MO(_FN), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
     _______, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
     _______, KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                    KC_RGUI, KC_RALT, KC_RCTL, KC_LEFT,          KC_DOWN, KC_RGHT
   ),
@@ -52,7 +52,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         SEND_STRING(SS_DOWN(X_RALT)"\""SS_UP(X_RALT)"a");
       }
-      break;
+      return true;
     case U_UMLAUT:
       if (record->event.pressed) {
         // when keycode QMKURL is pressed
@@ -224,23 +224,17 @@ static void render_fn(void) {
 }
 
 void oled_task_user(void) {
-    switch (get_highest_layer(layer_state)) {
-        case _MA:
-            /* oled_write_P(PSTR("Def\n"), false); */
-            render_ma();
-            break;
-        case _FN:
-            render_fn();
-            break;
-        default:
-            // Or use the write_ln shortcut over adding '\n' to the end of your string
-            oled_write_ln_P(PSTR("Undefined"), false);
-    }
-
-  /*   // Host Keyboard LED Status */
-  /*   led_t led_state = host_keyboard_led_state(); */
-  /*   oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false); */
-  /*   oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false); */
-  /*   oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false); */
+  switch (get_highest_layer(layer_state)) {
+    case _MA:
+      /* oled_write_P(PSTR("Def\n"), false); */
+      render_ma();
+      break;
+    case _FN:
+      render_fn();
+      break;
+    default:
+      // Or use the write_ln shortcut over adding '\n' to the end of your string
+      oled_write_ln_P(PSTR("Undefined"), false);
+  }
 }
 #endif
