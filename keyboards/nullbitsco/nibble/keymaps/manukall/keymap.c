6ba6f7a296cae0572f07652c61572eae39acfed3
Manuel Kallenbach <mk@manukall.de>
2021-03-25 08:36:48 +0100

    nibble: nav layer, up and down for volume

--- keyboards/nullbitsco/nibble/keymaps/manukall/keymap.c
+++ keyboards/nullbitsco/nibble/keymaps/manukall/keymap.c
@@ -17,6 +17,7 @@
 
 #define _MA 0
 #define _FN 1
+#define _NAV 2
 
 enum custom_keycodes {
   A_UMLAUT = SAFE_RANGE, U_UMLAUT, O_UMLAUT, SHARP_S};
@@ -28,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_MA] = LAYOUT_ansi(
             KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_ESC,
     KC_MUTE, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
-    KC_LGUI, MO(_FN), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
+    MO(_NAV), MO(_FN), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
     _______, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
     _______, KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                    KC_RGUI, KC_RALT, KC_RCTL, KC_LEFT,          KC_DOWN, KC_RGHT
   ),
@@ -36,7 +37,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_HOME,  KC_INS,
     RGB_TOG, _______, KC_HOME, KC_END, _______, _______, _______, _______, U_UMLAUT, _______, O_UMLAUT, _______, _______, _______, _______, _______,
     _______, MO(_FN), A_UMLAUT, SHARP_S, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______, _______,          _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, _______, _______, _______,          _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, _______, _______, _______,          KC_VOLU, _______,
+    _______, _______, _______, _______,                   _______,                   _______, _______, _______, _______,          KC_VOLD, _______
+  ),
+  [_NAV] = LAYOUT_ansi(
+               _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,  _______,  _______,  _______, _______,  _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    MO(_NAV), _______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, _______, _______, _______, _______,          _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
     _______, _______, _______, _______,                   _______,                   _______, _______, _______, _______,          _______, _______
   ),
 
@@ -231,6 +239,9 @@ void oled_task_user(void) {
       break;
     case _FN:
       render_fn();
+      break;
+    case _NAV:
+      oled_write_P(PSTR("NAV\n"), false);
       break;
     default:
       // Or use the write_ln shortcut over adding '\n' to the end of your string
